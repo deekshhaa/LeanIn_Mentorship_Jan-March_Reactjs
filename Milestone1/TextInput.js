a0d7f66a78528021d3fd2b50b84b3232a3d98974
deekshhaa <97409445+deekshhaa@users.noreply.github.com>
2022-02-06 00:54:34 +0530

    components

---
+++ Milestone1/TextInput.js
@@ -0,0 +1,9 @@
+const TextInput=()-> {
+return (
+<div>
+<h1> Hello Everyone!</h1>
+<h2> Hey !!!</h2>
+</div
+);
+}
+export default TextInput;
---
+++ Milestone1/Welcome.js
@@ -0,0 +1,8 @@
+const Welcome=()-> {
+return(
+<div>
+<h1> WELCOME!! to our server</h1>
+</div>
+);
+}
+export default Welcome;
