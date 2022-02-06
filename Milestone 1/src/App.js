23a944e7a1d72fd4181a24231d9143d890ea055c
deekshhaa <97409445+deekshhaa@users.noreply.github.com>
2022-02-06 22:32:13 +0530

    Add files via upload

---
+++ Milestone 1/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AddUsers from './components/users/AddUsers';
+
+
+function App(){
+  return(
+    <div>
+      <TextInput/>
+      <TextInput/>
+      <AddUsers/>
+
+    </div>
+  );
+}
+
+
+export default App;
+
+
